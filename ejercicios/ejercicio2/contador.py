508a679d37d900afcbc1adf696f9c68daf30fefd
lauralardies <lrodrlar@myuax.com>
2022-12-15 21:35:42 +0100

    arreglo ejercicio 2

--- ejercicios/ejercicio2/contador.py
+++ ejercicios/ejercicio2/contador.py
@@ -3,7 +3,10 @@ from io import open
 def existe(fichero):
     try:
         f = open(fichero, 'r')
+        contenido = f.read()
         f.close()
+        if len(contenido) == 0:
+            return False
     except FileNotFoundError:
         return False
 
@@ -17,13 +20,17 @@ def contador(*arg):
         fichero = open('ejercicios/ejercicio2/contador.txt', 'r')
         contenido = fichero.read()
         fichero.close()
-        if len(arg) != 0:
-            fichero = open('ejercicios/ejercicio2/contador.txt', 'w')
-            if 'inc' in arg:
-                contenido = int(contenido) + 1
-            elif 'dec' in arg:
-                contenido = int(contenido) - 1
-            fichero.write(str(contenido))
+        try:
+            contenido = int(contenido)
+            if len(arg) != 0:
+                fichero = open('ejercicios/ejercicio2/contador.txt', 'w')
+                if 'inc' in arg:
+                    contenido = int(contenido) + 1
+                elif 'dec' in arg:
+                    contenido = int(contenido) - 1
+                fichero.write(str(contenido))
+        except ValueError:
+            return 'Error: Fichero Corrupto.'
     fichero.close()
     return contenido
 
